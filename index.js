3779e62f21945e2a2b226508c696e59e17d9bc58
Diankrs30 <diankrs30@gmail.com>
2022-12-20 09:47:44 +0700

    set up remote notification (error)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import 'react-native-gesture-handler';
-import {AppRegistry, Platform} from 'react-native';
+import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import {Provider} from 'react-redux';
@@ -47,7 +47,7 @@ PushNotification.configure({
    * - if you are not using remote notification or do not have Firebase installed, use this:
    *     requestPermissions: Platform.OS === 'ios'
    */
-  requestPermissions: Platform.OS === 'ios',
+  requestPermissions: true,
 });
 
 // create channel for notification
